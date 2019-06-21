642398cdc53c812f2a859b24bb19f9b4fda6bbff
Olga2006 <37631339+Olga2006@users.noreply.github.com>
2019-06-21 13:35:24 +0200

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves-projet-angular';
+ constructor() {
+   const firebaseConfig = {
+     apiKey: "AIzaSyB4hG5eTlHF7ixpGbfdXWUqoOWB0GVBBCA",
+     authDomain: "bookshelves-projet-angular.firebaseapp.com",
+     databaseURL: "https://bookshelves-projet-angular.firebaseio.com",
+     projectId: "bookshelves-projet-angular",
+     storageBucket: "bookshelves-projet-angular.appspot.com",
+     messagingSenderId: "286399958507",
+     appId: "1:286399958507:web:77977358809e32b5"
+   };
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+
+ }
 }
